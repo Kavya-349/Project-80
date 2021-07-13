6d828c3cf5a3390350435a4ef0a2c9b735cce3a4
Kavya-349 <83696676+Kavya-349@users.noreply.github.com>
2021-07-13 21:32:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var list_of_names_array = [];
+
+function submit() {
+    document.getElementById("input_val").innerHTML = "";
+    var name = document.getElementById("input_val").value;
+    list_of_names_array.push(name);
+    console.log(list_of_names_array);
+    document.getElementById("display_names").innerHTML = list_of_names_array;
+}
+function shownames() {
+    var remove_commas = list_of_names_array.join("");
+    console.log(remove_commas);
+    document.getElementById("display_show_names").innerHTML = remove_commas;
+}
+function sorted() {
+    var sorted_val = list_of_names_array.join(" ");
+    sorted_val.sort();
+    console.log(sorted_val);
+    document.getElementById("display_sort_names").innerHTML = sorted_val;
+}
